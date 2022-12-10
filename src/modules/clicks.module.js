64df8d7a13268b6e9d95eb170f12953d85112f01
nova8 <aaa.auksutat@gmail.com>
2022-12-10 14:57:16 +0100

    Обработчик кликов

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,23 @@
 import {Module} from '../core/module'
 
 export class ClicksModule extends Module {
+  trigger() {
+    
+  }
+}
+// функция для счета количества кликов пользователя
+function startClickTracking() {
+  // счетчик кликов
+  let clickCount = 0;
 
+  // начинаем считать клики
+  document.addEventListener('click', function() {
+    clickCount++;
+  });
+
+  // ждем 5 секунд
+  setTimeout(function() {
+    // выводим статистику о количестве кликов
+    console.log(`Количество кликов: ${clickCount}`);
+  }, 5000);
 }
