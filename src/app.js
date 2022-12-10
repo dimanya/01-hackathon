fbac133b947f4bbbfa47d3dc85004b83a50db496
dimanya <dimanya35@gmail.com>
2022-12-10 20:40:35 +0500

    sound ready

--- src/app.js
+++ src/app.js
@@ -6,24 +6,7 @@ const contextMenu = new Menu('.menu')
 
 const soundModule = new SoundModule('sounds', 'Случайные звуки')
 contextMenu.add(soundModule.toHTML())
-/* import { Module } from './core/module'
-
-; */
-
-/* let module = new Module('clicks', 'Считать клики за 3 секунды');
-contextMenu.add(module.toHTML());
-
-module = new Module('figure', 'Создать фигуру');
-contextMenu.add(module.toHTML());
-
-module = new Module('color', 'Поменять цвет');
-contextMenu.add(module.toHTML());
-
-module = new Module('message', 'Вызвать сообщение');
-contextMenu.add(module.toHTML());
-
-module = new Module('timer', 'Таймер');
-contextMenu.add(module.toHTML()); */
+soundModule.trigger()
 
 
 
--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,10 +1,23 @@
 import {Module} from '../core/module'
+import { random } from '../utils';
+import AUDIO1 from '../assets/1.mp3'
+import AUDIO2 from '../assets/2.mp3'
+import AUDIO3 from '../assets/3.mp3'
+import AUDIO4 from '../assets/4.mp3'
+import AUDIO5 from '../assets/5.mp3'
 
 export class SoundModule extends Module {
     trigger() {
         const soundItem = document.querySelector('[data-type="sounds"]')
         soundItem.addEventListener('click', () => {
+
+            const audioArr = [AUDIO1, AUDIO2, AUDIO3, AUDIO4, AUDIO5]
             
+            const randomSoundNumber = random(0, 4)
+            
+            const audio = new Audio(audioArr[randomSoundNumber])
+            audio.play()
+
         })
 
         throw new Error(`Trigger method should be implemented in module "${this.type}"`)
