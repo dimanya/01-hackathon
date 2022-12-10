7b1409fe7457c66474321a073aa12539f1e1b90d
rodion1682 <rodiontropkin17@gmail.com>
2022-12-10 12:31:56 +0200

    Всплывающее окно

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import './styles.css'
 
+import { Menu } from './core/menu'
+
+const contextMenu = new Menu('.menu');
+
--- src/core/menu.js
+++ src/core/menu.js
@@ -1,23 +1,41 @@
 export class Menu {
-  constructor(selector) {
-    this.el = document.querySelector(selector)
+	constructor(selector) {
+	 this.el = document.querySelector(selector)
+ 
+	 document.body.addEventListener('contextmenu', event => {
+		 event.preventDefault();
+ 
+		 let x = event.offsetX;
+		 let y = event.offsetY;
 
-    document.body.addEventListener('click', event => {
-      if (event.target.offsetParent !== this.el) {
-        this.close()
-      }
-    })
-  }
+		 let windowWidth = window.innerWidth;
+		 let menuWidth = this.el.offsetWidth;
+		 let windowHeight = window.innerHeight;
+		 let menuHeight = this.el.offsetHeight;
+		 x = x > windowWidth - menuWidth ? windowWidth - menuWidth : x;
+		 y = y > windowHeight - menuHeight ? windowHeight - menuHeight : y;
 
-  open() {
-    throw new Error(`"open" method should be implemented in Menu"`)
-  }
-
-  close() {
-    throw new Error(`"close" method should be implemented in Menu"`)
-  }
-
-  add() {
-    throw new Error(`"add" method should be implemented in Menu"`)
-  }
-}
+		 this.open(x, y);
+	  });
+	  document.body.addEventListener('click', event => {
+		  if (event.target.offsetParent !== this.el) {
+			this.close();
+		 }
+	  })
+	}
+ 
+ 
+	open(left, top) {
+	 this.el.classList.add('open');
+	 this.el.style = `left: ${left}px; top: ${top}px;`;
+	}
+ 
+	close() {
+	 this.el.classList.remove('open');
+	}
+ 
+	add() {
+	  throw new Error(`"add" method should be implemented in Menu"`);
+	}
+ }
+ 
