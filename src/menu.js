5fc01f900f72d8dfacb7582748946417fcad5766
dotsenkodaria <dotsenko8268@gmail.com>
2022-12-11 18:39:33 +0200

    Реализация случайной фигуры

--- src/menu.js
+++ src/menu.js
@@ -1,81 +1,84 @@
-import { Menu } from './core/menu';
-import { Module } from './core/module';
-import { BackgroundModule } from './modules/background.module';
-import { ClicksModule } from './modules/clicks.module';
+import { Menu } from "./core/menu";
+import { Module } from "./core/module";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { FigureModule } from "./modules/figure.module";
 
 export class ContextMenu extends Menu {
-	constructor(selector) {
-		super(selector);
-		
-		document.body.addEventListener('contextmenu', event => {
-			event.preventDefault();
-	
-			let x = event.offsetX;
-			let y = event.offsetY;
-  
-			let windowWidth = window.innerWidth;
-			let menuWidth = this.el.offsetWidth;
-			let windowHeight = window.innerHeight;
-			let menuHeight = this.el.offsetHeight;
-  
-			x = x > windowWidth - menuWidth ? windowWidth - menuWidth : x;
-			y = y > windowHeight - menuHeight ? windowHeight - menuHeight : y;
-  
-			this.open(x, y);
-		});
-	}
+  constructor(selector) {
+    super(selector);
 
-	open(left, top) {
-		this.el.classList.add('open');
-		this.el.style = `left: ${left}px; top: ${top}px;`;
-	}
-	
-	close() {
-		this.el.classList.remove('open');
-	}
-	
-	add(item) {
-		this.el.innerHTML += item;
-	}
+    document.body.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+
+      let x = event.offsetX;
+      let y = event.offsetY;
+
+      let windowWidth = window.innerWidth;
+      let menuWidth = this.el.offsetWidth;
+      let windowHeight = window.innerHeight;
+      let menuHeight = this.el.offsetHeight;
+
+      x = x > windowWidth - menuWidth ? windowWidth - menuWidth : x;
+      y = y > windowHeight - menuHeight ? windowHeight - menuHeight : y;
+
+      this.open(x, y);
+    });
+  }
+
+  open(left, top) {
+    this.el.classList.add("open");
+    this.el.style = `left: ${left}px; top: ${top}px;`;
+  }
+
+  close() {
+    this.el.classList.remove("open");
+  }
+
+  add(item) {
+    this.el.innerHTML += item;
+  }
 }
 
-let newMenu = new ContextMenu('.menu');
+let newMenu = new ContextMenu(".menu");
 
-let module = new Module('clicks', 'Считать клики за 5 секунды');
+let module = new Module("clicks", "Считать клики за 5 секунды");
 newMenu.add(module.toHTML());
 
-module = new Module('figure', 'Создать фигуру');
+module = new Module("figure", "Создать фигуру");
 newMenu.add(module.toHTML());
 
-module = new Module('background', 'Поменять цвет');
+module = new Module("background", "Поменять цвет");
 newMenu.add(module.toHTML());
 
-module = new Module('message', 'Вызвать сообщение');
+module = new Module("message", "Вызвать сообщение");
 newMenu.add(module.toHTML());
 
-module = new Module('timer', 'Таймер');
+module = new Module("timer", "Таймер");
 newMenu.add(module.toHTML());
 
 const bodyElement = document.body;
-bodyElement.addEventListener('click', event => {
-	if (event.target.classList.contains('menu-item')) {
+bodyElement.addEventListener("click", (event) => {
+  if (event.target.classList.contains("menu-item")) {
+    let callModule;
+    let eventType = event.target.dataset.type;
+    let eventText = event.target.textContent;
 
-		let callModule;
-		let eventType = event.target.dataset.type;
-		let eventText = event.target.textContent;
-
-		if (eventType === 'background') {
-			callModule = new BackgroundModule(eventType, eventText);
-			callModule.trigger(bodyElement);
-		} else if (eventType === 'clicks') {
-			console.log('clicks');
-			callModule = new ClicksModule(eventType, eventText);
-			callModule.trigger(bodyElement);
-		} //else if (eventType === 'timer') {
-		//	console.log(bodyElement);
-		//	callModule = new TimerModule(eventType, eventText);
-		//	callModule.trigger(bodyElement);
-		//}
-	
-	}
+    if (eventType === "background") {
+      callModule = new BackgroundModule(eventType, eventText);
+      callModule.trigger(bodyElement);
+    } else if (eventType === "clicks") {
+      console.log("clicks");
+      callModule = new ClicksModule(eventType, eventText);
+      callModule.trigger(bodyElement);
+    } else if (eventType === "figure") {
+      console.log("figure");
+      callModule = new FigureModule(eventType, eventText);
+      callModule.trigger(bodyElement);
+    } else if (eventType === "timer") {
+      console.log(bodyElement);
+      callModule = new TimerModule(eventType, eventText);
+      callModule.trigger(bodyElement);
+    }
+  }
 });
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,11 +1,17 @@
-import {Module} from '../core/module'
-import {random} from '../utils'
+import { Module } from "../core/module";
+import { ContextMenu } from "../menu";
+import { random } from "../utils";
 
 export class BackgroundModule extends Module {
-	constructor(type, text) {
-		super(type, text);
-	}
-	trigger(item) {
-		return item.style.background = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
-	}
+  constructor(type, text) {
+    super(type, text);
+  }
+  trigger(item) {
+    item.style.background = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(
+      0,
+      255
+    )})`;
+    const contextMenu = new ContextMenu(".menu");
+    contextMenu.close();
+  }
 }
