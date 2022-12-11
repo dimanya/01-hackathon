40e72a0c8d4a122f1b9d267f5d424e99f175b70c
dimanya <dimanya35@gmail.com>
2022-12-11 17:37:28 +0500

    random sounds done

---
+++ src/modules/sound.module.js
@@ -0,0 +1,29 @@
+import {Module} from '../core/module'
+import { ContextMenu } from '../menu'
+import { random } from '../utils'
+import AUDIO1 from '../assets/1.mp3'
+import AUDIO2 from '../assets/2.mp3'
+import AUDIO3 from '../assets/3.mp3'
+import AUDIO4 from '../assets/4.mp3'
+import AUDIO5 from '../assets/5.mp3'
+
+export class SoundModule extends Module {
+    constructor(type, text) {
+		super(type, text);
+	}
+    trigger() {
+        const soundItem = document.querySelector('[data-type="sounds"]')
+        soundItem.addEventListener('click', () => {
+
+            const audioArr = [AUDIO1, AUDIO2, AUDIO3, AUDIO4, AUDIO5]
+            
+            const randomSoundNumber = random(0, 4)
+            
+            const audio = new Audio(audioArr[randomSoundNumber])
+            audio.play()
+
+            const contextMenu = new ContextMenu('.menu')
+            contextMenu.close()
+        })
+      }
+}
